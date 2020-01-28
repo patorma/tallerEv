f9376e16a26606b7cb376f6cd01b7b0df06c4a39
Mauricio Ibañez Deana <mauricio.ibanez.deana@everis.com>
2020-01-28 12:50:15 -0300

    Conectamos Brazo y agrego msg Input

--- src/app/components/brazo/brazo.component.ts
+++ src/app/components/brazo/brazo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-brazo',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brazo.component.css']
 })
 export class BrazoComponent implements OnInit {
+
+  @Input() mensaje = 'msg por defecto';
 
   constructor() { }
 
