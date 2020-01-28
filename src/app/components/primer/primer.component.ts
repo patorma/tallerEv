c11593f5ea95b467793f041324bffede8fe47792
Mauricio Ibañez Deana <mauricio.ibanez.deana@everis.com>
2020-01-28 11:19:49 -0300

    Agregamos components
    primer agregado.

---
+++ src/app/components/primer/primer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primer', // ajustado
+  templateUrl: './primer.component.html', // ajustado
+  // styleUrls: ['./app.component.css']  - no lo usamos
+})
+export class PrimerComponent {
+  title = 'proyectito';
+}
