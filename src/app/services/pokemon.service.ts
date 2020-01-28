40d7aa728881cd3a6cd8da107757dd663d028c1f
Mauricio Ibañez Deana <mauricio.ibanez.deana@everis.com>
2020-01-28 15:49:43 -0300

    Agregamos método GET cliente que obtiene json de Pokemones

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -7,4 +7,9 @@ import { HttpClient } from '@angular/common/http';
 export class PokemonService {
 
   constructor(private http: HttpClient) { }
+
+  getPokemon() {
+    return this.http.get('https://ng-pokedex.firebaseapp.com/api/pokemon.json');
+  }
+
 }
