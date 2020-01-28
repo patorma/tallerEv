777e6c2ec160cd000609abd3b4bdc77a2b2898e3
Mauricio Ibañez Deana <mauricio.ibanez.deana@everis.com>
2020-01-28 12:57:44 -0300

    Agregamos Output en componente hijo Brazo

--- src/app/components/brazo/brazo.component.ts
+++ src/app/components/brazo/brazo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-brazo',
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BrazoComponent implements OnInit {
 
   @Input() mensaje = 'msg por defecto';
-
+  @Output() enviar = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
