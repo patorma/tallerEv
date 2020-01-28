c3a9d64b061f294f4d4b1d0c0a7d5e5f1d4ba6e1
Mauricio Ibañez Deana <mauricio.ibanez.deana@everis.com>
2020-01-28 11:27:05 -0300

    Agregamos componente.
    app.module.ts agrega módulo
    app.component.html - agregamos tag a componente
    primer.component.html - agregamos texto html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrimerComponent } from './components/primer/primer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponent // agregado (arriba el import igual)
   ],
   imports: [
     BrowserModule
